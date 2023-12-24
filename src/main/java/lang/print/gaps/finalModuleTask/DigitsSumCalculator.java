777e7025a57fb04e6ddd868c8b60784770b7fc97
Martynas <martynas.bartnykas@gmail.com>
2023-12-24 12:57:36 +0200

    Final task for module 3!

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,17 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        String value = String.valueOf(number);
+        char [] digits = value.toCharArray();
+
+        int sum = 0;
+
+        for (char digitChar : digits){
+            int digit = Character.getNumericValue(digitChar);
+            sum += digit;
+        }
+
+        System.out.println(sum);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+
+        float temperatureFahrenheit = ((float)temperatureCelsius*9F/5F) + 32F;
+
+        System.out.println(temperatureFahrenheit);
     }
 }
